77f34731142216fb3b879681bd9b6b444c029ccd
Connie Zhou <omnawmrawr@gmail.com>
2018-11-10 16:36:51 -0500

    traffic

--- controllers/image.js
+++ controllers/image.js
@@ -1,32 +1,20 @@
 const Clarifai = require('clarifai');
-
-//You must add your own API key here from Clarifai.
-const app = new Clarifai.App({
- apiKey: '5afcdd03318441ba8d1cdccdb1672d27'
-});
-/*
-{
-    "input": "https://vignette.wikia.nocookie.net/marvelcinematicuniverse/images/b/bb/Tony_Stark_Promo.jpg/revision/latest?cb=20141129202546"
+let app = new Clarifai.App({apiKey: 'fa72cd380d3e4218b151b2f97d3a7e73'});
+const traffic = (req, res) => {
+  app.models.predict({id:'Traffic-Detection', version:'99e6c3c31d1747de9d778911bcd95ca0'}, req.body.input).then(
+    function(response) {
+      let status = response.outputs[0].data.concepts[0].id;
+      let percentage = response.outputs[0].data.concepts[0].value;
+      let returnResp = status + ";" + percentage;
+      res.send(returnResp);
+    },
+    function(err) {
+      // there was an error
+    }
+  );
 }
-*/
-const handleApiCall = (req, res) => {
-  app.models
-    .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-    .then(data => {
-      res.json(data);
-    })
-    .catch(err => res.status(400).json('unable to work with API'))
-}
-
-app.models.predict(Clarifai.GENERAL_MODEL, 'https://samples.clarifai.com/metro-north.jpg').then(
-  function(response) {
-    console.log(response);
-  },
-  function(err) {
-    console.error(err);
-  }
-);
 
 module.exports = {
-  handleApiCall
+  traffic
 };
+
